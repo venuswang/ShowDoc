d47cfca9552a73700b72290c822aa154980d690b
venuswang <1823889319@qq.com>
2016-07-02 10:56:26 +0800

    增加了后台的验证码生成Control

--- src/cn/scau/edu/ssm/showdoc/controller/VoucherInfoController.java
+++ src/cn/scau/edu/ssm/showdoc/controller/VoucherInfoController.java
@@ -1,11 +1,17 @@
 package cn.scau.edu.ssm.showdoc.controller;
 
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -68,6 +74,18 @@ public class VoucherInfoController {
 			model.addAttribute("voucherVO", voucherVO);
 			return "login/register";
 		}
+		/**
+		 * 使用后台自己的验证码
+		 */
+		/*
+		 * String code = request.getSession().getAttribute("vcode");
+		 * if(!code.equals(!request.getParameter("code")))
+		 * {
+		 * 		model.addAttribute("errors", "验证码出错");
+		 *		model.addAttribute("voucherVO", voucherVO);
+		 * 	    return "login/register";
+		 * }
+		 */
 		if(pic != null && pic.getOriginalFilename() != null && pic.getOriginalFilename().length() > 0) 
 		{
 			String path = "F:\\picture\\";
@@ -160,6 +178,79 @@ public class VoucherInfoController {
 	
 	
 	/**
+	 * 获取验证码
+	 */
+	@RequestMapping(value="/getCaptchar.action",method={RequestMethod.GET,RequestMethod.POST})
+	public void getCaptchar(HttpServletRequest request,HttpServletResponse response) throws Exception
+	{
+		response.setContentType("image/jpeg");    
+        response.setHeader("Pragma", "No-cache");    
+        response.setHeader("Cache-Control", "no-cache");    
+        response.setDateHeader("Expires", 0);    
+        HttpSession session = request.getSession();    
+        // 在内存中创建图象    
+        int width = 75, height = 25;    
+        BufferedImage image = new BufferedImage(width, height,    
+                BufferedImage.TYPE_INT_RGB);    
+        // 获取图形上下文    
+        Graphics g = image.getGraphics();    
+        // 生成随机类    
+        Random random = new Random();    
+        // 设定背景色    
+        g.setColor(getRandColor(200, 250));    
+        g.fillRect(0, 0, width, height);    
+        // 设定字体    
+        g.setFont(new Font("Times New Roman", Font.PLAIN, 24));    
+        // 画边框    
+        g.setColor(getRandColor(160, 200));    
+        g.drawRect(0, 0, width - 1, height - 1);    
+        // 随机产生155条干扰线，使图象中的认证码不易被其它程序探测到    
+        g.setColor(getRandColor(160, 200));    
+        for (int i = 0; i < 155; i++) {    
+            int x = random.nextInt(width);    
+            int y = random.nextInt(height);    
+            int xl = random.nextInt(12);    
+            int yl = random.nextInt(12);    
+            g.drawLine(x, y, x + xl, y + yl);    
+        }    
+        // 取随机产生的认证码(4位数字)    
+        String sRand = "";    
+        for (int i = 0; i < 4; i++) {    
+            String rand = String.valueOf(random.nextInt(10));    
+            sRand += rand;    
+            // 将认证码显示到图象中    
+            g.setColor(new Color(20 + random.nextInt(110), 20 + random    
+                    .nextInt(110), 20 + random.nextInt(110)));    
+            // 调用函数出来的颜色相同，可能是因为种子太接近，所以只能直接生成    
+            g.drawString(rand, 13 * i + 14, 20);    
+        }    
+    
+        // 将认证码存入SESSION    
+        session.setAttribute("vcode", sRand);    
+    
+        // 图象生效    
+        g.dispose();    
+        // 输出图象到页面    
+        ImageIO.write(image, "JPEG", response.getOutputStream()); 
+	}
+	
+	/**
+	 * 获取验证码的内容(具有实时性)
+	 */
+	@RequestMapping(value="/getVcode.action",method={RequestMethod.GET,RequestMethod.POST})
+	public void getVcode(HttpServletRequest request,HttpServletResponse response) throws Exception
+	{
+		String code = (String)request.getSession().getAttribute("vcode");
+		if(code == null || "".equals(code.trim()))
+			code = "3490"; 
+		response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = null;
+        out = response.getWriter();
+        out.println(code);  //前端接收时记得有\n
+	}
+	
+	/**
 	 * 通过ID来查询用户的个人信息
 	 */
 	@RequestMapping(value="/queryVoucherById.action",method={RequestMethod.GET,RequestMethod.POST})
@@ -212,4 +303,19 @@ public class VoucherInfoController {
 		request.getSession().invalidate();
 		return "forward:jsp/login/exit.jsp";
 	}
+	
+	/**
+	 * 获取验证码的颜色
+	 */
+	Color getRandColor(int fc, int bc) {// 给定范围获得随机颜色    
+        Random random = new Random();    
+        if (fc > 255)    
+            fc = 255;    
+        if (bc > 255)    
+            bc = 255;    
+        int r = fc + random.nextInt(bc - fc);    
+        int g = fc + random.nextInt(bc - fc);    
+        int b = fc + random.nextInt(bc - fc);    
+        return new Color(r, g, b);    
+    }    
 }
